12e42438fc065069b1a48dfc0185025fc487314f
jpjorge <jpjorge@eac68a8d-a220-0410-892d-49a7c2af8522>
2006-12-08 00:18:41 +0000

    -> using the Tori and already some nice colors

--- trunk/AnisotropicShader/AnisotropicShader.cpp
+++ trunk/AnisotropicShader/AnisotropicShader.cpp
@@ -5,13 +5,13 @@
 #include <math.h>
 #include <GL/glew.h>
 #include <GL/glut.h>
-//#include <IL/il.h>
-//#include <IL/ilu.h>
-//#include <IL/ilut.h>
 
 #include "ShaderAttributeValue.h"
 #include "ShaderObject.h"
 #include "ShaderProgram.h"
+
+#include "CMesh.h"
+#include "3ds.h"
 
 /************************************************************************/
 /* PROGRAM GLOBAL VARIABLES                                             */
@@ -23,17 +23,27 @@ char titlestring[200];
 GLint lightDirectionLoc;
 
 // the last parameter, 0,  makes it become a directional light
-GLfloat lightDirection[] = {0.0f, 0.0f, 1.0f, 0.0f};
+GLfloat lightDirection[] = {0.0f, 0.5f, 1.0f, 0.0f};
 
 // rotation angle
-float angle;
+float rotationAngle;
 
-ShaderProgram* shaderProgram;
-ShaderObject* vertexShader;
-ShaderObject* fragmentShader;
+ShaderProgram* toonShaderProgram;
+ShaderObject* toonVertexShader;
+ShaderObject* toonFragmentShader;
 
-//GLuint textureID;
-//ShaderAttributeValue<int> texAttrib;
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
+GLfloat torusDiffuse[3][4] = {{0.0f, 0, 0.4f, 1.0}, {0.3f, 0.4f, 0.3f, 1.0}, {0.6f, 0, 0, 1.0}};
+GLfloat torusSpecular[3][4] = {{1, 1, 1, 1.0}, {1, 1, 1, 1.0}, {1, 1, 1, 1.0}};
+GLfloat torusShininess[3] = {32, 128, 64};
+
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
+
+CMesh* model = NULL;
 
 void showFPS();
 
@@ -42,15 +52,15 @@ void showFPS();
 */
 void initShaders()
 {
-	vertexShader = new ShaderObject(GL_VERTEX_SHADER, "./shaders/phong.vert");
-	fragmentShader = new ShaderObject(GL_FRAGMENT_SHADER, "./shaders/phong.frag");
+	toonVertexShader = new ShaderObject(GL_VERTEX_SHADER, "./shaders/phong.vert");
+	toonFragmentShader = new ShaderObject(GL_FRAGMENT_SHADER, "./shaders/phong.frag");
 
-	shaderProgram = new ShaderProgram();
+	toonShaderProgram = new ShaderProgram();
 
-	shaderProgram->attachShader( *vertexShader );
-	shaderProgram->attachShader( *fragmentShader );
+	toonShaderProgram->attachShader( *toonVertexShader );
+	toonShaderProgram->attachShader( *toonFragmentShader );
 
-	shaderProgram->buildProgram();
+	toonShaderProgram->buildProgram();
 }
 
 /*
@@ -60,6 +70,9 @@ void init()
 {
 	glEnable(GL_CULL_FACE); // Cull away all back facing polygons
 	glEnable(GL_DEPTH_TEST); // Use the Z buffer
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightDirection); // Set light position
 
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
@@ -67,7 +80,10 @@ void init()
 	// to load correctly the extensions
 	initShaders();
 
-	angle = 0;
+	Load3ds loader;
+	model = loader.Create("./data/torus.3ds");
+
+	rotationAngle = 0;
 }
 
 /*
@@ -88,39 +104,54 @@ void reshape(int w, int h)
 * drawScene(float t) - the actual drawing commands to render our scene.
 */
 void drawScene() {
-	glLightfv(GL_LIGHT0, GL_POSITION, lightDirection); // Set light position
-	GLfloat diffuse[4] = {0.8f, 0, 0, 1.0};
-	GLfloat specular[4] = {1, 1, 1, 1.0};
-	//GLfloat light_pos[4] = {0, 0, 1, 0};
-	GLfloat shininess = 128;
+	glPushMatrix();
+	{
+		glTranslatef(0, 0, -2);
 
-	// since we are scaling (in our particular case, uniform scaling) 
-	// the object, the normals need to be rescaled
-	glEnable(GL_RESCALE_NORMAL);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	//glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
-	glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+		/* ######## Torus 1 ######### */
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, torusDiffuse[0]);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, torusSpecular[0]);
+		glMaterialf(GL_FRONT, GL_SHININESS, torusShininess[0]);
 
+		glPushMatrix();
+		{
+			glTranslatef(0, 0, -3);
+			glRotatef(45, 0, 1, 0);
+			glRotatef(rotationAngle, 1, 0, 0);
+			glutSolidTorus(0.25f, 2.0f, 32, 32);
+		}
+		glPopMatrix();
 
-	// Enable lighting and the LIGHT0 we placed before
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
+		/* ######## Torus 2 ######### */
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, torusDiffuse[1]);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, torusSpecular[1]);
+		glMaterialf(GL_FRONT, GL_SHININESS, torusShininess[1]);
 
-	glPushMatrix(); // Transforms to animate the object:
+		glPushMatrix();
+		{
+			glTranslatef(0, 0, -3);
+			glRotatef(-45, 0, 1, 0);
+			glRotatef(rotationAngle, 1, 1, 0);
+			glutSolidTorus(0.2f, 1.6f, 32, 32);
+		}
+		glPopMatrix();
 
-	//glutSolidSphere(0.3f, 32, 32);
-	glRotatef(angle, -1, 0, 0);
-	glutSolidTorus(0.1f, 0.3f, 32, 32);
+		/* ######## Torus 3 ######### */
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, torusDiffuse[2]);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, torusSpecular[2]);
+		glMaterialf(GL_FRONT, GL_SHININESS, torusShininess[2]);
 
-	glPopMatrix(); // Revert to initial transform
+		glPushMatrix();
+		{
+			glTranslatef(0, 0, -3);
+			glRotatef(35, 0, 1, 0);
+			model->draw();
+		}
+		glPopMatrix();
+	}
 
-	// Disable lighting again, to prepare for next frame.
-	glDisable(GL_LIGHTING);
-	glDisable(GL_LIGHT0);
-
+	glPopMatrix();
+	
 	showFPS();
 }
 
@@ -152,30 +183,33 @@ void display()
 	// Clear the color buffer and the depth buffer.
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	shaderProgram->useProgram();
+	toonShaderProgram->useProgram();
 
 	// Render with the shaders active.
 	drawScene();
 
 	// Deactivate the shaders.
-	shaderProgram->disableProgram();
+	toonShaderProgram->disableProgram();
 
 	glutSwapBuffers();
 }
 
+
 /*
 *	Idle function
 */
-void anim()
+void anim(int millis)
 {
+	
 	// update the rotation angle
-	angle += 0.05f;
+	rotationAngle += 1.5f;
 
-	if (angle > 360.0f)
+	if (rotationAngle > 360.0f)
 	{
-		angle -= 360.0f;
+		rotationAngle -= 360.0f;
 	}
 	glutPostRedisplay();
+	glutTimerFunc(millis, anim, millis);
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -199,7 +233,7 @@ int main(int argc, char** argv)
 	init();
 	glutReshapeFunc(reshape);
 	glutDisplayFunc(display);
-	glutIdleFunc(anim); 
+	glutTimerFunc(20, anim, 20);
 	glutKeyboardFunc(keyboard);
 	glutMainLoop();
 
